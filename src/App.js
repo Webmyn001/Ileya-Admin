5cbcfbcb6065debad84b3ad32b80c19e3e313d61
Webmyn001 <bellomuhyideen0001@gmail.com>
2024-03-10 18:45:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import {BrowserRouter as Router , Route, Routes, } from "react-router-dom";
 import Contact from './Ajadi/Client/Contact'
 import Home from './Ajadi/Client/Home'
 import Footer from './Ajadi/Client/Footer/Footer'
-import Navbar from './Ajadi/Admin/header/Navbar'
-import Admin from './Ajadi/Admin/Admin';
-import Details from './Ajadi/Admin/Details';
+import Navbar from './Ajadi/Client/header/Navbar'
+import Admin from './Ajadi/Client/Admin';
+import Details from './Ajadi/Client/Details';
 import IDcardAdmin from './Ajadi/Client/IDcardAdmin';
-import Message from './Ajadi/Admin/Message';
 
 
 
@@ -17,25 +16,12 @@ function App() {
  
   return (
     <div className='font-josefins bg-[#f5f5f5]'>
-       <Router>
-            <Navbar/>
        
+       <Router>
+    <Navbar/>
          <Routes>
-               {/* Admin */}
-               <Route path = "/" exact element= {<Admin/>}></Route>
-               <Route path = "/details/:id" exact element= {<Details/>}></Route>
-               <Route path = "/message" exact element= {<Message/>}></Route>
-
-
-              
-
-
-
-
-
-
-
-               
+               <Route path = "/" exact element= {<Home/>}></Route>
+               <Route path = "/contact" exact element= {<Contact/>}></Route>
          </Routes>
        <Footer/>
          </Router> 
