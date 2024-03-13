09c2a5a46aa9cad9d2b5e0d97596bbaca16d843f
Webmyn001 <bellomuhyideen0001@gmail.com>
2024-03-14 00:43:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {BrowserRouter as Router , Route, Routes, } from "react-router-dom";
 import Contact from './Ajadi/Client/Contact'
 import Home from './Ajadi/Client/Home'
 import Footer from './Ajadi/Client/Footer/Footer'
 import Navbar from './Ajadi/Client/header/Navbar'
-import Admin from './Ajadi/Client/Admin';
-import Details from './Ajadi/Client/Details';
+import axios from 'axios';
 
 
 
 
 function App() {
 
+  const [Message, setMessage] =useState([])
+
+
+  const getMessage = async  () => {
+    axios.get("https://crazy-gown-cow.cyclic.app/api/msg/")
+    .then(res => {
+        console.log(res.data)
+      setMessage(res.data)
+    }).catch(err => {
+      console.log(err)
+    })
+   }
+
+   useEffect(() => {
+    getMessage()
+   
+    }, [])
  
   return (
     <div className='font-josefins bg-[#f5f5f5]'>
@@ -19,7 +35,7 @@ function App() {
        <Router>
     <Navbar/>
          <Routes>
-               <Route path = "/" exact element= {<Home/>}></Route>
+               <Route path = "/" exact element= {<Home  Message={Message} />}></Route>
                <Route path = "/contact" exact element= {<Contact/>}></Route>
          </Routes>
        <Footer/>
